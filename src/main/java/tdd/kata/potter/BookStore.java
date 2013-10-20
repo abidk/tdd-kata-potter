f537b62466e9352997d466ce9afa2239aa94ce09
abidk <Abid>
2013-10-20 10:23:09 +0100

    RED: Test fails when buying one book.

---
+++ src/main/java/tdd/kata/potter/BookStore.java
@@ -0,0 +1,15 @@
+package tdd.kata.potter;
+
+import java.math.BigDecimal;
+
+public class BookStore {
+
+  public void buy() {
+
+  }
+
+  public BigDecimal total() {
+    return BigDecimal.ZERO;
+  }
+
+}
